627ea37a456f61e74a1b4f5afb8389d80298ce43
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-02-07 19:51:03 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+  a <=> b 
+  end
+end
 
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+  b <=> a 
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+  a.length <=> b.length 
+  end
+end
+
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1] 
+  array
+end
+
+def reverse_array(array)
+   array.reverse 
+end
+
+def kesha_maker(array)
+    array.each { |elem| elem[2] = "$"}
+end 
+
+
+def find_a(array)
+  array.select{|string| string.start_with?("a")}
+end 
+
+def sum_array(array)
+  sum = 0 
+  array.inject(0) { |sum, a| sum + a }
+end 
+
+def add_s(array)
+  array.collect { |elem| 
+   if array[1] == elem
+     elem 
+    else 
+    elem + "s"
+  end  
+  }
+end 
